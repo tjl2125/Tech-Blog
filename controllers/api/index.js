6fb324aca5e2b9fb6ee82c6a283dcaa44025dcc9
Taylor Leong <tjl2125@alum.barnard.edu>
2021-05-09 19:37:07 -0400

    goddang syntax error in my index file fixed the route

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const postRoutes = require('./postRoutes');
-const commentRoutes = require ('./commentRoutes'); 
+const commentRoutes = require ('./commentRoute'); 
 
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
